d6703edc83296d08900165dcf5212127683cf4e5
Aadesh <adeshnewaskar@gmail.com>
2025-06-07 14:37:05 +0530

    Initial Commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by Adesh Newaskar on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDTests/CICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTests.swift
+//  CICDTests
+//
+//  Created by Adesh Newaskar on 07/06/25.
+//
+
+import Testing
+@testable import CICD
+
+struct CICDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
